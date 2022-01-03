7d6864caa3a2993f2d74e88083f5238414c38d6b
Ahmad <aksoftware98@hotmail.com>
2022-01-03 04:20:47 +0400

    Start writing the logic of the app

---
+++ src/AKSoftware.ExpensesAnalyzer/AKSoftware.ExpensesAnalyzer.Services/Interfaces/IExpensesService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKSoftware.ExpensesAnalyzer.Services.Interfaces
+{
+    /// <summary>
+    /// Interface that wraps all the logic against the expenses object
+    /// </summary>
+    public interface IExpensesService
+    {
+
+        /// <summary>
+        /// Add a new expense object
+        /// </summary>
+        /// <returns></returns>
+        Task CreateAsync();
+
+    }
+}
